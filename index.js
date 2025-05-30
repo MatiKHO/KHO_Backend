6b6398db621e928708725c57fe4f841963083d22
MatiKHO <matiasproint1@gmail.com>
2025-05-30 10:26:33 +0200

    Updated changes in packagejson

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5500;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('API Kumiho operativa'));
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+})
---
+++ src/controllers/userController.js
@@ -0,0 +1,82 @@
+import prisma from "../prisma/index.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "El usuario ya existe" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(201).json({
+      message: "Usuario registrado con éxito",
+      user: newUser,
+    });
+  } catch (error) {
+    console.error("Error al registrar el usuario:", error);
+    res.status(500).json({ message: "Error al registrar el usuario" });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Usuario no encontrado, credenciales incorrectas" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Credenciales inválidas" });
+    }
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({ message: "Inicio de sesión exitoso", token });
+  } catch (error) {
+    console.error("Error al iniciar sesión:", error);
+    res.status(500).json({ message: "Error al iniciar sesión" });
+  }
+};
+
+export const userProfile = async (req, res) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: req.user.id },
+      select: { id: true, fullName: true, email: true },
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "Usuario no encontrado" });
+    }
+
+    res.status(200).json({ message: "Perfil del usuario encontrado", user });
+  } catch (error) {
+    res.status(500).json({ message: "Error al obtener el perfil del usuario" });
+  }
+};
