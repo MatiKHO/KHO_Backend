659084aa0e19faa6e52d248aadc61091e684493f
MatiKHO <matiasproint1@gmail.com>
2025-06-03 10:13:07 +0200

    Updated register and login routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import userRoutes from './src/routes/user.route.js';
 
 dotenv.config();
 
@@ -14,6 +15,9 @@ app.use(cors({
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('API Kumiho operativa'));
+
+// Routes
+app.use("/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
